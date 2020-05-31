ded08f52808a5ef0a3851385d52eb1d84c56e94a
Chris Pasquali <chrispasquali@Chriss-MacBook-Pro.local>
2020-05-31 16:00:35 +1000

    pushing to remote

--- crm1/settings.py
+++ crm1/settings.py
@@ -143,7 +143,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'email'
+EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 
 # To allow less secure apps - https://myaccount.google.com/lesssecureapps 
